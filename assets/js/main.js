e6396c2060b08a3e2d34042c8c35fe32468470d6
lucas-quirino <luksquirino@edu.unirio.br>
2022-12-13 14:31:21 -0300

    criando repositório

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("side-nav").style.width = "65vw";
+  }
+
+  function closeNav() {
+    document.getElementById("side-nav").style.width = "0";
+  }
---
+++ readme.md
@@ -0,0 +1,1 @@
+Vercel link: https://guarana-landing-ox3lm1dp8-lucas-quirino-dev.vercel.app/
